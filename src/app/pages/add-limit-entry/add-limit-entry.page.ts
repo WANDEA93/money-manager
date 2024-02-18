9a22ce62c3efd955cddc203a1f0e080d6031bb7c
Asitha De Alwis <w.dealwis@ucdconnect.ie>
2024-02-18 21:50:04 +0530

    Added new form for entering expenditure entry

--- src/app/pages/add-limit-entry/add-limit-entry.page.ts
+++ src/app/pages/add-limit-entry/add-limit-entry.page.ts
@@ -1,20 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {IonicModule} from '@ionic/angular';
+import {addIcons} from "ionicons";
+import {checkmark, close} from "ionicons/icons";
+import {MonthlyLimitsService} from "../../services/monthly-limits.service";
+import {Router} from "@angular/router";
+import {ExpenditureEntry} from "../../models/expenditure-entry";
 
 @Component({
   selector: 'app-add-limit-entry',
   templateUrl: './add-limit-entry.page.html',
   styleUrls: ['./add-limit-entry.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule]
 })
-export class AddLimitEntryPage implements OnInit {
+export class AddLimitEntryPage {
 
-  constructor() { }
+  public form: FormGroup;
 
-  ngOnInit() {
+  constructor(private limitService: MonthlyLimitsService,
+              private router: Router) {
+
+    addIcons({checkmark, close})
+    this.form = new FormGroup({
+      amount: new FormControl(),
+      date: new FormControl(new Date().toDateString()),
+      vendor: new FormControl(),
+      comment: new FormControl()
+    });
   }
+
+  public onSave(): void {
+    const payload: any = this.form.value;
+    if(payload['date'] === undefined){
+      payload['date'] = new Date();
+    }
+
+    this.limitService.addEntry(payload as ExpenditureEntry);
+
+  }
+
+  public onCancel(): void {
+    this.router.navigate(['/home']); //TODO: should be back
+  }
+
 
 }
