aba2fecd4398a24dae54c8acad774c47853d69cb
Asitha De Alwis <w.dealwis@ucdconnect.ie>
2024-02-23 20:05:21 +0530

    Fixed error data not loading when app initialised

--- src/app/pages/monthly-limits/monthly-limits.page.ts
+++ src/app/pages/monthly-limits/monthly-limits.page.ts
@@ -3,10 +3,10 @@ import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {IonicModule} from '@ionic/angular';
 import {LimitItemComponent} from "../../components/limit-item/limit-item.component";
-import {MonthlyLimitDetail, MonthlyLimitHeader} from "../../models/monthly-limit";
+import {MonthlyLimitHeader} from "../../models/monthly-limit";
 import {ToolbarComponent} from "../../components/toolbar/toolbar.component";
 import {MonthlyLimitsViewService} from "../../services/monthly-limits-view.service";
-import {ActivatedRoute, ParamMap} from "@angular/router";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-monthly-limits',
@@ -21,29 +21,12 @@ export class MonthlyLimitsPage implements OnInit {
 
   constructor(private monthlyLimitsViewService: MonthlyLimitsViewService,
               private activatedRoute: ActivatedRoute) {
-
   }
 
   ngOnInit() {
-
     this.monthlyLimitsViewService.selectedHeader.subscribe((header) => {
       this.header = header;
     });
-
-    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
-      if (!paramMap.has('id')) {
-        return;
-      }
-
-      const id: string = paramMap.get('id')!;
-      if (id === 'active') {
-        this.monthlyLimitsViewService.selectActiveHeader()
-      } else {
-        this.monthlyLimitsViewService.selectPreviousHeader(id);
-      }
-    });
-
-
   }
 
   public getPageTitle(): string {
--- src/app/services/monthly-limits-view.service.ts
+++ src/app/services/monthly-limits-view.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {MonthlyLimitDetail, MonthlyLimitHeader} from "../models/monthly-limit";
-import {Observable, share, shareReplay, Subject} from "rxjs";
+import {BehaviorSubject, delay, Observable, publish, ReplaySubject, share, shareReplay, Subject} from "rxjs";
 import {MonthlyLimitsService} from "./monthly-limits.service";
 
 @Injectable({
@@ -8,13 +8,13 @@ import {MonthlyLimitsService} from "./monthly-limits.service";
 })
 export class MonthlyLimitsViewService {
 
-  private _selectedHeader$: Subject<MonthlyLimitHeader> = new Subject<MonthlyLimitHeader>();
-  public selectedHeader: Observable<MonthlyLimitHeader> = this._selectedHeader$.asObservable().pipe(shareReplay(1));
+  private _selectedHeader$: ReplaySubject<MonthlyLimitHeader> = new ReplaySubject<MonthlyLimitHeader>();
+  public selectedHeader: Observable<MonthlyLimitHeader> = this._selectedHeader$.asObservable().pipe();
 
-  private _selectedDetail$: Subject<MonthlyLimitDetail> = new Subject<MonthlyLimitDetail>();
+  private _selectedDetail$: ReplaySubject<MonthlyLimitDetail> = new ReplaySubject<MonthlyLimitDetail>();
   public selectedDetail: Observable<MonthlyLimitDetail> = this._selectedDetail$.asObservable().pipe(shareReplay(1));
 
-  private _selectedPreviousHeaders$: Subject<MonthlyLimitHeader[]> = new Subject<MonthlyLimitHeader[]>();
+  private _selectedPreviousHeaders$: ReplaySubject<MonthlyLimitHeader[]> = new ReplaySubject<MonthlyLimitHeader[]>();
   public selectedPreviousHeaders: Observable<MonthlyLimitHeader[]> = this._selectedPreviousHeaders$.asObservable().pipe(shareReplay(1));
 
   private _showAddEntry: boolean = false;
